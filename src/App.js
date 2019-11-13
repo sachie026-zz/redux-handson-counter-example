2ba848339573e3567ef98291caa53c5e8e6f7a3d
Sachin Jadhav <sachinjadhav@News-MacBook-Pro.local>
2019-11-13 22:25:02 +0530

    added basic store, reducer and actions for the redux setup

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import './App.css';
 import { createStore } from 'redux';
 import Counter from "./components/counter";
 
-function reducer(state, action) {
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
   console.log('reducer', state, action);
-  return state;
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 }
 
 const store = createStore(reducer);
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
 
 function App() {
   return (
