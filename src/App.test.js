feb02a5db5df18491032407512920b6802326c97
Sachin Jadhav <jadhavsachin174@gmail.com>
2019-11-23 21:34:56 +0530

    added unti test for action creator: reset

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,26 @@
 import  { increment, decrement , INCREMENT, DECREMENT}  from "./redux-store/actions"
+import * as rt from "./redux-store/reset-actions"
 
 describe("action creator test", () => {
-  it("action creator for increment", () => {
+  it("action creator test for increment", () => {
     const testINC = {
       type: INCREMENT
     }
     Object.is(increment(), testINC);
   })
 
-  it("action creator for decrement", () => {
+  it("action creator test for decrement", () => {
     const testINC = {
       type: DECREMENT
     }
     Object.is(decrement(), testINC);
+  })
+
+  it("action creator test for resrt", () => {
+    const resetTest = {
+      type: rt.RESET
+    }
+
+    Object.is(rt.reset(), resetTest)
   })
 })
